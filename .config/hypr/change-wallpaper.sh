4106d4553324817aca9cf687fdb31a8b7f43e8f5
Andreas Simon <andreas.w.simon@gmail.com>
2026-03-22 14:41:48 +0100

    Update Hypr config for new version

--- .config/hypr/change-wallpaper.sh
+++ .config/hypr/change-wallpaper.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+# The link is used in Hyprpaper's config
+LINK="$HOME/.config/hypr/wallpaper.png"
+CURRENT_WALL=$(readlink "$LINK")
 # Get the name of the focused monitor with hyprctl
 FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
+hyprctl hyprpaper wallpaper "$FOCUSED_MONITOR","$WALLPAPER",cover
 
 # Set it as default for the next hyprpaper start
-ln -sf "$WALLPAPER" "$HOME/.config/hypr/wallpaper.png"
+ln -sf "$WALLPAPER" "$LINK"
